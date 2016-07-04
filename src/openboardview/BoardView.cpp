927e7eac5572f7143f0ab6d95895a68438d29800
piernov <piernov@piernov.org>
2016-07-04 20:08:47 +0200

    Asus boardview files (.fz) support Thanks a lot to @pafmaf for the decrypting stuff! More info here: https://github.com/chloridite/OpenBoardView/issues/10

--- src/openboardview/BoardView.cpp
+++ src/openboardview/BoardView.cpp
@@ -13,6 +13,7 @@
 #include "BRDBoard.h"
 #include "BRDFile.h"
 #include "BDVFile.h"
+#include "FZFile.h"
 #include "imgui/imgui.h"
 
 #include "NetList.h"
@@ -131,6 +132,8 @@ int BoardView::LoadFile( char *filename ) {
           file = new BRDFile(buffer, buffer_size);
         else if (!strcmp(ext, ".bdv"))
           file = new BDVFile(buffer, buffer_size);
+        else if (!strcmp(ext, ".fz"))
+          file = new FZFile(buffer, buffer_size);
 
         if (file && file->valid) {
           SetFile(file);
